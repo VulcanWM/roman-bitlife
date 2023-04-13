112c0ce09de6f7914a0953cd044b4a09c652bbb5
VulcanWM <logmedha09@gmail.com>
2023-04-13 11:30:55 +0100

    added light and dark mode

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Text, View, TouchableOpacity } from "./components/Themed";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
---
+++ components/Themed.js
@@ -0,0 +1,46 @@
+import {
+    Text as DefaultText,
+    useColorScheme,
+    View as DefaultView, 
+    TouchableOpacity as DefaultTouchableOpacity
+  } from "react-native";
+  
+import Colors from "../constants/Colors";
+
+export function useThemeColor(props, colorName) {
+const theme = useColorScheme();
+const colorFromProps = props[theme];
+
+if (colorFromProps) {
+    return colorFromProps;
+} else {
+    return Colors[theme][colorName];
+}
+}
+
+export function Text(props) {
+    const { style, lightColor, darkColor, ...otherProps } = props;
+    const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+
+    return <DefaultText style={[{ color }, style]} {...otherProps} />;
+}
+
+export function View(props) {
+    const { style, lightColor, darkColor, ...otherProps } = props;
+    const backgroundColor = useThemeColor(
+        { light: lightColor, dark: darkColor },
+        "background"
+    );
+
+    return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+}
+
+export function TouchableOpacity(props) {
+    const { style, lightColor, darkColor, ...otherProps } = props;
+    const backgroundColor = useThemeColor(
+        { light: lightColor, dark: darkColor },
+        "background"
+    );
+
+    return <DefaultTouchableOpacity style={[{ backgroundColor }, style]} {...otherProps} />;
+}
